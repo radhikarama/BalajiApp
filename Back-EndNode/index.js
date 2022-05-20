4cd83783fdc3cce5641d92adf590d9834840fb80
radhikarama <74363007+radhikarama@users.noreply.github.com>
2022-05-20 14:50:21 +0530

    changes for deployment

--- Back-EndNode/index.js
+++ Back-EndNode/index.js
@@ -3,20 +3,21 @@ const globalConfig = require('./utils/globalConfig')
 const Controller = require('./Controllers/controller')
 const dbConn = require('./utils/dbConfig')
 const cors = require('cors');
-const bodyparser = require('body-parser')
-const task = require('./Services/task')
+const bodyparser = require('body-parser');
+const path = require('path');
+
 
 const app = express();
 app.use(bodyparser.json());
 app.use(express.json());
 app.use(cors());
-task();
+app.use(express.static(path.join(Client + "/public")))
 const objCollection = {
  app: app
 }
 new Controller(objCollection);
 
 const server = require('http').createServer(app)
-server.listen(process.env.port || global.config.PORT, () => {
+server.listen(process.env.PORT || global.config.PORT, () => {
  console.log(`listing to the port ${global.config.PORT}`)
 })
