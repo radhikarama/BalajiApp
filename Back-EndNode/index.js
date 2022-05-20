a4a0247a3f9c7957dc0c8bdf1ddd707c3065c654
radhikarama <74363007+radhikarama@users.noreply.github.com>
2022-05-20 22:03:25 +0530

    changes

--- Back-EndNode/index.js
+++ Back-EndNode/index.js
@@ -5,21 +5,20 @@ const dbConn = require('./utils/dbConfig')
 const cors = require('cors');
 const bodyparser = require('body-parser');
 const path = require('path');
-
+require('dotenv').config();
 
 const app = express();
 app.use(bodyparser.json());
 app.use(express.json());
 app.use(cors());
 const PORT = process.env.PORT || global.config.PORT;
-if (process.env.NODE_ENV === 'production') {
+if (process.env.PORT === 'production') {
  app.use(express.static('Client/build'))
 }
 const objCollection = {
  app: app
 }
 new Controller(objCollection);
-//process.env.PORT ||
 const server = require('http').createServer(app)
 server.listen(PORT, () => {
  console.log(`listing to the port ${global.config.PORT}`)
