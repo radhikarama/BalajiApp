e2f25054abf89db8542dcc2a661d0a206749097f
radhikarama <74363007+radhikarama@users.noreply.github.com>
2022-06-02 10:16:44 +0530

    mongodb connection changed

--- utils/globalConfig.js
+++ utils/globalConfig.js
@@ -1,4 +1,5 @@
 config = {}
 
 config.PORT = 5000;
-config.Connection = "mongodb://localhost:27017/Sweets"
+//config.Connection = "mongodb://localhost:27017/Sweets"
+config.Connection = "mongodb+srv://radhika:radhika123@cluster0.qrfj37o.mongodb.net/Sweets"
